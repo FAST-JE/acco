421b58988d9c008bed88fbfcb41535ba4443bd63
FAST <fast-jag@yandex.ru>
2017-03-11 10:11:10 +0300

    second cm

--- main.js
+++ main.js
@@ -1,36 +1,37 @@
-function acco() {
-    document.addEventListener("DOMContentLoaded", function() {
-        var acco = document.getElementById("acco");
+document.addEventListener("DOMContentLoaded", function() {
+    function acco() {
+            var acco = document.getElementById("acco");
 
-        acco.addEventListener("click", function(event) {
+            acco.addEventListener("click", function(event) {
 
-            var target = event.target;
+                var target = event.target;
 
-            if(target.classList.contains("acco__trigger")) {
+                if(target.classList.contains("acco__trigger")) {
 
-                var content = target.nextElementSibling,
-                    textBlock = content.firstElementChild,
-                    textHeight = textBlock.clientHeight,
-                    item = target.parentNode,
-                    items = [].slice.call(acco.children);
+                    var content = target.nextElementSibling,
+                        textBlock = content.firstElementChild,
+                        textHeight = textBlock.clientHeight,
+                        item = target.parentNode,
+                        items = [].slice.call(acco.children);
 
-                event.preventDefault();
+                    event.preventDefault();
 
-                if(!item.classList.contains("active")) {
-                    items.forEach(function(elem) {
-                        elem.classList.remove("active");
-                        elem.lastElementChild.style.height = 0;
-                    });
-                    item.classList.add("active");
-                    content.style.height = textHeight + "px";
-                } else {
-                    item.classList.remove("active");
-                    content.style.height = 0;
+                    if(!item.classList.contains("active")) {
+                        items.forEach(function(elem) {
+                            elem.classList.remove("active");
+                            elem.lastElementChild.style.height = 0;
+                        });
+                        item.classList.add("active");
+                        content.style.height = textHeight + "px";
+                    } else {
+                        item.classList.remove("active");
+                        content.style.height = 0;
+                    }
+
+
                 }
+            });
+    }
+    acco();
 
-
-            }
-        });
-    });
-}
-acco();
+});
